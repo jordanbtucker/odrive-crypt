0d556f3988cff24cf7f80d4c0a7646d52c52fe90
Jordan Tucker <jordanbtucker@gmail.com>
2017-03-25 12:32:11 -0500

    Add bin test for single file

--- test/bin-tests.js
+++ test/bin-tests.js
@@ -7,21 +7,38 @@ const tap = require('tap')
 
 const fixturesDir = path.join(__dirname, 'fixtures')
 const rootDir = path.join(fixturesDir, 'root')
+const rootFile = path.join(fixturesDir, 'MaUi8C8YFNhN5dzbjNtqgXx3Cm-PwyUYxR1Rl4JauHHrjrTMo0k9poE=')
 const outputDir = path.join(fixturesDir, 'output')
 const outputRootDir = path.join(outputDir, 'root')
 
 try { fs.mkdirSync(outputDir) } catch (err) {}
 try { fs.mkdirSync(outputRootDir) } catch (err) {}
 
-process.argv = [
-	'node', 'bin.js',
-	'--passphrase', 'password123',
-	'-i', rootDir,
-	'-o', outputRootDir,
-]
-
 tap.test('cli', t => {
 	t.test('should decrypt a folder', t => {
+		delete require.cache[require.resolve('../src/bin')]
+
+		process.argv = [
+			'node', 'bin.js',
+			'--passphrase', 'password123',
+			'-i', rootDir,
+			'-o', outputRootDir,
+		]
+
+		require('../src/bin')
+		t.end()
+	})
+
+	t.test('should decrypt a file', t => {
+		delete require.cache[require.resolve('../src/bin')]
+
+		process.argv = [
+			'node', 'bin.js',
+			'--passphrase', 'password123',
+			'-i', rootFile,
+			'-o', outputDir,
+		]
+
 		require('../src/bin')
 		t.end()
 	})
